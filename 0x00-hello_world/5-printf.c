840bc4a574ff896b16974491da68816d986dc9c3
Okeomasilachi <okeomasilachi@gmail.com>
2023-02-16 17:48:02 -0800

    create 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Prints "with proper grammar, but the outcome is a piece of art,"
+ * main - Prints "with proper grammar, but the outcome is a piece of art,i"
  *
  * Return: (0) as final output
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
