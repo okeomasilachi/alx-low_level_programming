c61ca9457ff98fd8d9fb83d1a2fef1c55f90da17
Okeomasilachi <okeomasilachi@gmail.com>
2023-02-17 18:48:46 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* main - Print combinations of two digit numbers
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int a, b, c;
+
+	for (a = '0'; a < '9'; a++)
+	{
+		for (b = a + 1; b <= '9'; b++)
+			for (c = b + 1; c <= '9'; c++)
+			{
+				if ((b != a) != c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a == '7' && b == '8')
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+	}
+	putchar('\n');
+
+	return (0);
+}
