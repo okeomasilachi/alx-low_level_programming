d8184bb1b86b9c1a5dada63acf1444edb8d09c4f
Okeomasilachi <okeomasilachi@gmail.com>
2023-02-19 15:22:19 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,12 @@
-#include <unistd.h> 
+#include <unistd.h>
 
+/**
+ * main - the entrypoint
+ *
+ * return - the programe returns 1
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return(1);
+	return (1);
 }
