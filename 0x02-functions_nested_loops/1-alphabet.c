b92092ecdcd05d7d4f4cd59a50b2fe572d44f70c
Okeomasilachi <okeomasilachi@gmail.com>
2023-02-21 15:38:27 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Print the alphabet in lowercase.
+ *
+ * Return: void.
+ */
+
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 97; a < 123; a++)
+		{
+		_putchar(a);
+		}
+		_putchar('\n');)
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,23 @@
-#include <unistd.h>
+#ifndef MAIN_H_
+#define MAIN_H_
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+void pr_digit(int charcode, int fmt);
+int add(int a, int b);
+void pr_with_fmt(int charcode, int fmt);
+void print_to_98(int n);
+void pr_3_digit(int charcode, int fmt);
+void print_times_table(int n);
+void print_spaces(int n);
+void print_zero(void);
+
+#endif
