c4e3be6d5944b96129c659b6d34a26f136528a49
Okeomasilachi <okeomasilachi@gmail.com>
2023-02-21 12:55:32 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "_putchar.c"
+
+/**
+ * main - entry point of program
+ *
+ * Return: success
+ */
+int main(void)
+{
+	int o;
+	char k[] = "_putchar";
+
+		for (o = 0; o < 8; o++)
+			{
+			_putchar(o[k]);
+			}
+			_putchar('\n');
+		return (1);
+}
