c63d1540ad5acc2f1a0f266809ccfdc332eed2ad
Okeomasilachi <okeomasilachi@gmail.com>
2023-02-23 10:11:40 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -11,14 +11,12 @@ int _isupper(int c)
 {
 	char c;
 
-	if (c >= 69 && c <= 90)
+	if (c >= 65 && c <= 90)
 	{
-		printf("%c: ", c);
 		return (1);
 	}
 	else
 	{
-		printf("%c: ", c);
 		return (0);
 	}
 }
