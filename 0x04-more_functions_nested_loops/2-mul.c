b8aa10bd82e485ca10562c69797e9cfc9565d0f1
Okeomasilachi <okeomasilachi@gmail.com>
2023-02-24 08:12:18 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 digits.
+ * @a: viriable to be multiplies
+ * @b: variable to be multiplied
+ *
+ * Return: 1 c is a number, 0 otherwise.
+ */
+
+int mul(int a, int b)
+{
+	int result = (a * b);
+
+	return (result);
+}
