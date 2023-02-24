a1cd046206b084d338778444a71f6d985911e3ed
Okeomasilachi <okeomasilachi@gmail.com>
2023-02-24 15:39:02 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - prints the character _ the number of times pecified
+ * @n: specified number of times
+ * Return: success 0
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+	if (n > 0)
+	{
+		while (i <= n)
+		{
+			_putchar('_');
+			i++;
+		}
+	_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
