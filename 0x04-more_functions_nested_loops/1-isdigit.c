1ce2f34ce9b348757df8efeaecd41964876ec6c2
Okeomasilachi <okeomasilachi@gmail.com>
2023-02-24 07:42:02 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _isupper - Checks for uppercase Alphabets.
+* @c: The parameter to be checked.
+*
+* Return: 1 if Alphabet is uppercase, 0 otherwise.
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
