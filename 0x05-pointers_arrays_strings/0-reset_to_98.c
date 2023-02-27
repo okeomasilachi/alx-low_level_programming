a932ef3b9d1e4088fb5bf1f182f7d7f8b8ae67e0
Okeomasilachi <okeomasilachi@gmail.com>
2023-02-27 13:59:28 -0800

    create file 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes variable n and resets to 98
+ * @n: value to be reset
+ * Return: always success
+ */
+
+void reset_to_98(int *n)
+{
+	int *p = n;
+
+	*p = 98;
+}
