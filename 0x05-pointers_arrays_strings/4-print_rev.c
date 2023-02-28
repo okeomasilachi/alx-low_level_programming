ad54330b18ee101ec877f9eb3d8b61615aefa5b2
Okeomasilachi <okeomasilachi@gmail.com>
2023-02-28 11:15:56 -0800

    create file 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in revers
+ * @s: tring to be printed
+ *
+ * Return: success
+ */
+
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+	_putchar('\n');
+}
