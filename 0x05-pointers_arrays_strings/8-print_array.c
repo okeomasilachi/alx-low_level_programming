756aac0fc2cef59f1af587f040738851cbbf3285
Okeomasilachi <okeomasilachi@gmail.com>
2023-02-28 16:51:37 -0800

    create file 8-print_array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_array - print the n value of an array
+ * @a: name of array
+ * @n: n term to print
+ *
+ * Return: success
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		printf("%d", a[i]);
+
+		if (i == n - 1)
+			continue;
+		printf(", ");
+
+	printf("\n");
+}
