94c038d3706c03f351ffafb4ff7a7fa4a6c19ef8
Okeomasilachi <okeomasilachi@gmail.com>
2023-02-28 16:55:45 -0800

    create file 8-print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,11 +13,15 @@ void print_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < n; i++)
+	{
 		printf("%d", a[i]);
 
 		if (i == n - 1)
+		{
 			continue;
+		}
 		printf(", ");
+	}
 
 	printf("\n");
 }
