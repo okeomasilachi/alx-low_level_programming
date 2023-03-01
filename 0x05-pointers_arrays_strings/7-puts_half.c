75da9271d1783f5ddadac562599cd47df9094f07
Okeomasilachi <okeomasilachi@gmail.com>
2023-02-28 16:23:28 -0800

    create file 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - cuts input to 2and print later half
+ * @str: input
+ *
+ * Return: success
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int lan = 0;
+	int n;
+
+	while (str[i++])
+		lan++;
+	if ((lan % 2) == 0)
+		n = lan / 2;
+	else
+		n = (lan - 1) / 2;
+	for (i = n; i < lan; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
