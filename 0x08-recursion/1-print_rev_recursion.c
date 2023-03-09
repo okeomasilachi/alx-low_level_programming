b7475bf20b342c3c401b881703dd462b08905549
Okeomasilachi <okeomasilachi@gmail.com>
2023-03-09 01:31:48 -0800

    create file 1-print_rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - entry point. printsa string in revers
+ * @s: string to be reversed
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
