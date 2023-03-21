eee58f2771655bf89336ce023ee1521cf16b37c7
Okeomasilachi <okeomasilachi@gmail.com>
2023-03-21 15:35:57 -0700

    create file 3-function_like_macro.c

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#include <stdlib.h>
-#define ABS(x) abs(x)
+#define ABS(x) ((x) < 0 ? (-x): (x))
 
 #endif /* FUNCTION_LIKE_MACRO_H */
