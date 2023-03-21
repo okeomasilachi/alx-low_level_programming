d63613cc95020d0fd7a05e50661e43f2fe5beec5
Okeomasilachi <okeomasilachi@gmail.com>
2023-03-21 14:44:50 -0700

    create file 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name os the souce file the 
+ *		program was complied form
+ *
+ * Return: void
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
