82d009da11c747783b8cf639f622ff708e803cf5
Okeomasilachi <okeomasilachi@gmail.com>
2023-03-25 10:03:08 -0700

    ok

--- 0x0E-structures_typedef/main.c
+++ 0x0E-structures_typedef/main.c
@@ -6,14 +6,9 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-    struct dog my_dog;
-
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+    init_dog(NULL, "Poppy", 3.5, "Bob");
+    printf("My name is");
     return (0);
 }
