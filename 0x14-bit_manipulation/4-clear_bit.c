552db17193e9caf3bd7f1f598c6c7f25266ddf37
Okeomasilachi <okeomasilachi@gmail.com>
2023-04-10 19:22:03 +0100

    write function that sets a bit to 0

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * set_bit - function sets bit to 1
+ * @n:  parameter to set
+ * @index: index to set
+ *
+ * Return: on success 1 else -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int k = index;
+
+	*n &= ~(BV << k);
+
+	if (*n << k == '0')
+		return (1);
+	else
+		return (-1);
+}
