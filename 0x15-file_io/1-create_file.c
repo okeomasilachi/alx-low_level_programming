8cb683d47b7a83a691efb2589dbccae3601d5223
Okeomasilachi <okeomasilachi@gmail.com>
2023-04-14 17:27:43 +0100

    Create a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,13 +14,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (*filename)
 	{
-		if (filename == "NULL")
-			return (-1);
-
 		fb = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 		wt = write(fb, text_content, mlen);
 		close(fb);
+
+		return (1);
 	}
 
-	return (1);
+	return (-1);
 }
