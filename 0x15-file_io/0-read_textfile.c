1df6022fc1bb8f82b8cbbb6bab9e4e86c77c0282
Okeomasilachi <okeomasilachi@gmail.com>
2023-05-08 20:30:48 +0100

    reads and prints to stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,9 +3,9 @@
 /**
  * read_textfile - reads and prints to stdout a particular number of byte
  * @filename: file to read from
- * @letters: number of bytes to read
+ * @letters: number of bytes to read and written
  *
- * Return: on success the number of bytes read
+ * Return: on success number of bytes written
  * error 0
 */
 
@@ -15,33 +15,34 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t red, wrt;
 	char *buf = malloc(sizeof(char) * letters);
 
-	if (filename != NULL)
-	{
-		fd = open(filename, O_RDONLY);
-
-		if (fd == -1)
-		{
-			free(buf);
-			return (0);
-		}
-		red = read(fd, buf, letters);
-		if (red == -1)
-		{
-			close(fd);
-			free(buf);
-			return (0);
-		}
-		wrt = write(STDOUT_FILENO, buf, letters);
-		if (wrt == -1)
-		{
-			close(fd);
-			free(buf);
-			return (0);
-		}
-		close(fd);
-		free(buf);
-		return (red);
-	}
-	else
+	if (filename == NULL)
 		return (0);
+
+	fd  = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	red = read(fd, buf, letters);
+	if (red == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	wrt = write(STDOUT_FILENO, buf, letters);
+	if (wrt == -1 || wrt != red)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	free(buf);
+	close(fd);
+	return (wrt);
 }
