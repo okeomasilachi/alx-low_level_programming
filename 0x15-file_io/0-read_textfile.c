eb611449c9e0c695affe30c23505fff98049d0f7
Okeomasilachi <okeomasilachi@gmail.com>
2023-05-08 20:07:37 +0100

    edited the read_textfile file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t red, wrt;
 	char *buf = malloc(sizeof(char) * letters);
 
-	if (filename)
+	if (filename != NULL)
 	{
 		fd = open(filename, O_RDONLY);
 
