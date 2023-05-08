830e8fa800ac82943e5f21be14e94c8ef401ad08
Okeomasilachi <okeomasilachi@gmail.com>
2023-05-08 21:48:35 +0100

    kk

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,25 +9,6 @@
 #include <errno.h>
 
 
-/**
- * _strlen_recursion - entry point of he program
- * @s: the string to perform operation on
- * Return: returns the lenght of a srting
- */
-
-int _strlen(char *s)
-{
-	int i = 0;
-
-	if (*s)
-	{
-		i++;
-		i += _strlen(s + 1);
-	}
-
-	return (i);
-}
-
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
