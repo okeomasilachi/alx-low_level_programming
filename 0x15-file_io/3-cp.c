e6d6525d2c353d184daab3a3f640f1e145165197
Okeomasilchi <110942635+Okeomasilchi@users.noreply.github.com>
2023-05-09 13:48:30 +0100

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,30 +12,26 @@ int main(int ac, char **av)
 {
 	int ff, ft, ftc, ffc;
 	char *buf;
-	ssize_t nr;
+	ssize_t nr, nw;
 
 	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+
 	ft = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (ft == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
 	ff = open(av[1], O_RDONLY);
-	if (ff == -1)
-	{
-		close(ft);
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
 	buf = malloc(BUF_OKI);
 	while ((nr = read(ff, buf, BUF_OKI)) > 0)
+		nw = write(ft, buf, nr);
+	
+	if (nr == -1 || ff == -1)
 	{
-		write(ft, buf, nr);
+		close(ft);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	}
+	if (nw == -1 || ft == -1)
+	{
+		close(ff);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);		
 	}
 	free(buf);
 	ffc = close(ff), ftc = close(ft);
