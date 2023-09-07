d0a881cc2b8d3d52922fecb6d532af14d8ffacee
Okeomasilachi <okeomasilachi@gmail.com>
2023-09-07 22:24:19 +0100

    modify hash table get funtion

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,21 +10,28 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *cpy = NULL;
+	char *ret = NULL;
+	unsigned long idx;
 
 	if (ht == NULL || key == NULL)
 		return (NULL);
 
-	cpy = ht->array[key_index((unsigned char *) key, ht->size)];
+	idx = key_index((unsigned char *) key, ht->size);
+	cpy = ht->array[idx];
 
-	if (cpy == NULL)
+	if (ht->array[idx] == NULL)
 		return (NULL);
 
-	while (cpy->next != NULL)
+	while (ht->array[idx] != NULL)
 	{
-		if (strcmp(cpy->key, key) == 0)
-			return (strdup(cpy->value));
-
-		cpy = cpy->next;
+		if (strcmp(ht->array[idx]->key, key) == 0)
+		{
+			ret = ht->array[idx]->value;
+			ht->array[idx] = cpy;
+			return (ret);
+		}
+		ht->array[idx] = ht->array[idx]->next;
 	}
+	ht->array[idx] = cpy;
 	return (NULL);
 }
