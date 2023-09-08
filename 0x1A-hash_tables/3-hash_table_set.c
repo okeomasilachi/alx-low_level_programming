77cbad6195ecdb007bc83d502494fd227902d5d3
Okeomasilachi <okeomasilachi@gmail.com>
2023-09-08 16:51:52 +0100

    delete function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -37,6 +37,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			if (strcmp(ht->array[index]->key, key) == 0)
 			{
+				free(ht->array[index]->value);
 				ht->array[index]->value = strdup(value);
 				ht->array[index] = cur;
 				return (1);
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,53 +1,30 @@
 #include "hash_tables.h"
 
-
-/**
- * function - recursively frees memory
- *			allocated for a linked list of hash nodes.
- * @list: The parameter "list" is a pointer to a hash_node_t structure.
- * 
- * Return: nothing, as indicated by the "void" return type.
- */
-void _free(hash_node_t *list)
-{
-	if (list == NULL)
-		return;
-
-	_free(list->next);
-	free(list->key);
-	free(list->value);
-	free(list);
-}
-
 /**
  * hash_table_delete - Delete's a hash table and
- * free the memory allocated for
- * it.
- * 
- * @param ht The parameter "ht" is a pointer to a hash table structure.
- * 
- * @return In the given code, if the `ht` parameter is `NULL`, the function will return without
- * performing any further actions. If `ht` is not `NULL`, the function will free the memory allocated
- * for the hash table and its elements. However, the function does not explicitly return any value.
+ *				free the memory allocated for it.
+ * @ht: pointer to a hash table structure.
+ *
+ * Return: Void
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned lont int i;
-
-	if (ht == NULL)
-		return;
+	size_t i;
+	hash_node_t *del = NULL, *nxt = NULL;
 
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i] == NULL)
-			continue;
+		del = ht->array[i];
+		while (del != NULL)
+		{
+			nxt = del->next;
+			free(del->key);
+			free(del->value);
+			free(del);
 
-		if (ht->array[i] != NULL)
-			_free(ht->array[i]);
+			del = nxt;
+		}
 	}
-	for (i = 0; i < ht->size; i++)
-		free(ht->array[i]);
-	
 	free(ht->array);
 	free(ht);
 }
