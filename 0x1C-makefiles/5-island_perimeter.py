6df72b5a2581f973a8d799d028830116a8c884c0
Okeomasilachi <okeomasilachi@gmail.com>
2023-10-26 00:43:47 +0100

    Technical interview preparation:

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -15,7 +15,7 @@ def island_perimeter(grid):
             described in grid
     """
     peri = 0
-    
+
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
@@ -24,5 +24,5 @@ def island_perimeter(grid):
                     peri -= 2
                 if j > 0 and grid[i][j-1] == 1:
                     peri -= 2
-    
+
     return peri
