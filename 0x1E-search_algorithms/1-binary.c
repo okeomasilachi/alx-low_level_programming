7e14b15222f39d737f9cefabd492f76c57416e43
Okeomasilachi <okeomasilachi@gmail.com>
2023-12-14 19:09:44 +0100

    binary_search - performs a linear search on an array to find a specific value.

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -23,7 +23,7 @@ int binary_search(int *array, size_t size, int value)
 	while (left <= right)
 	{
 		mid = left + (right - left) / 2;
-		printf("Searching in array: ");
+		printf("Searching in array:");
 		for (i = left; i < right; i++)
 			printf(" %d,", array[i]);
 		printf(" %d\n", array[i]);
