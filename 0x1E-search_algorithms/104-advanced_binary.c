6c866d6d5f85688c2da32d567570cb8c99ecb5c1
Okeomasilachi <okeomasilachi@gmail.com>
2023-12-15 10:26:30 +0100

    Write a function that searches for a value in a sorted array of integers.
    Prototype : int advanced_binary(int *array, size_t size, int value);
    Where array is a pointer to the first element of the array to search in
    size is the number of elements in array
    And value is the value to search for
    Your function must return the index where value is located
    You can assume that array will be sorted in ascending order
    If value is not present in array or if array is NULL, your function must return -1
    Every time you split the array, you have to print the new array (or subarray) you’re searching in (See example)
    You have to use recursion. You may only use one loop (while, for, do while, etc.) in order to print the array

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,20 +1,71 @@
 #include "search_algos.h"
 
 /**
+ * print_array - Prints the elements of an array between
+ *               two indices (left and right).
+ * @array: A pointer to the first element of the array.
+ * @left: The starting index.
+ * @right: The ending index.
+ */
+void print_array(int *array, size_t left, size_t right)
+{
+	size_t i;
+
+	printf("Searching in array:");
+	for (i = left; i < right; i++)
+		printf(" %d,", array[i]);
+	printf(" %d\n", array[i]);
+}
+
+/**
  * advanced_binary - performs a linear search on an array
- *					to find a specific value.
+ *                   to find a specific value.
  * @array: A pointer to the first element of the array
- *			to be searched.
+ *         to be searched.
  * @size: The size parameter represents the number of
- *			elements in the array. It is of type size_t,
- *			wrch is an unsigned integer type.
- * @value: The value you are searcrng for in the array.
+ *        elements in the array. It is of type size_t,
+ *        which is an unsigned integer type.
+ * @value: The value you are searching for in the array.
  *
  * Return: value else NOT_FOUND if value not in array
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	(void)array, (void)size, (void)value;
+	size_t left = 0, right = (size - 1), mid;
+	size_t i;
+
+	if (array == NULL || size == 0)
+		return (NOT_FOUND);
+
+	while (left <= right)
+	{
+		mid = left + (right - left) / 2;
+		print_array(array, left, right);
+
+		if (array[mid] == value && array[mid - 1] != value)
+			return (mid);
+		else if (array[mid] == value && array[mid - 1] == value)
+		{
+			print_array(array, left, mid);
+
+			i = mid;
+			do {
+				mid--;
+			} while (array[mid] == value);
+
+			left = mid + 1;
+			mid = i;
+
+			print_array(array, left, mid);
+
+			return (--mid);
+		}
+
+		if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
 
 	return (NOT_FOUND);
 }
